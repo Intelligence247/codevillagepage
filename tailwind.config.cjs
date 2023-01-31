6d2009b87dff36ec6cdc85048e00fa6231851a39
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-31 09:21:19 +0100

    fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1b734f"
+      }
+    },
   },
   plugins: [],
 }
